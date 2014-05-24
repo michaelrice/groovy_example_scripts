c48cd8e0374d94ab57ea36012a6a50cca06aed50
Michael Rice <michael@michaelrice.org>
2014-05-24 03:11:57 -0500

    Update byte_play.groovy

--- byte_play.groovy
+++ byte_play.groovy
@@ -6,7 +6,7 @@ buffer[3] = 54
 
 long filesize = 0L
 buffer.eachByte {
-    filesize = filesize * 10L + (long) (new String(it).minus(0) as int)
+    filesize = filesize * 10L + (long) (new String(it) as int)
 }
 
 println filesize
