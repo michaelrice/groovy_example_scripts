81708b84a7f2125858cab5c524c56bf820d04f74
Michael Rice <michael.rice@rackspace.com>
2014-06-22 21:46:53 -0500

    modifications to convert whole site for mediawiki script

--- xml_mediawiki_to_github.groovy
+++ xml_mediawiki_to_github.groovy
@@ -1,15 +1,29 @@
-def foo = new XmlSlurper().parse(new File('/home/errr/Downloads/fluxbox-wiki/Category_Howtos/Fluxbox-wiki-20140621231421.xml'))
-def pages = foo.page
-println pages.size()
+List files = [
+        "Fluxbox-wiki-br-howtos.xml",
+        "Fluxbox-wiki-en-howtos.xml",
+        "Fluxbox-wiki-fin-howtos.xml",
+        "Fluxbox-wiki-it-howtos.xml",
+        "Fluxbox-wiki-ru-howtos.xml",
+        "Fluxbox-wiki-de-howtos.xml",
+        "Fluxbox-wiki-es-howtos.xml",
+        "Fluxbox-wiki-fr-howtos.xml",
+        "Fluxbox-wiki-ko-howtos.xml"
+]
+files.each {
+    def foo = new XmlSlurper().parse(new File("/home/errr/Downloads/${it}"))
+    def pages = foo.page
+    println pages.size()
 
-String title
-pages.each {
-    title = it.title
-    title = title.replaceAll(" ","_")
-    title = title.replaceAll("/", "-")
-    title = title.replaceAll(":", "-")
-    tfile = new File("/home/errr/programs/fluxbox-wiki.org/${title}")
-    tfile.withWriter { out ->
-        out.write(it.revision.text)
+    String title
+    String countrycode = it.toString().split("-")[2]
+    pages.each { wikipage ->
+        title = wikipage.title
+        title = title.replaceAll(" ", "_")
+        title = title.replaceAll("/", "-")
+        title = title.replaceAll(":", "-")
+        tfile = new File("/home/errr/programs/fluxbox-wiki.org/category/howtos/${countrycode}/${title}")
+        tfile.withWriter { out ->
+            out.write(wikipage.revision.text)
+        }
     }
 }
