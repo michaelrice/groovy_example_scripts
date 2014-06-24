2ef95c80226d6f81e2700c93326b381692154c80
Michael Rice <michael.rice@rackspace.com>
2014-06-24 01:47:05 -0500

    added esxcli xml parsing example that returns a map of the openmanage install info if found

---
+++ xml_esxcli_hardware.groovy
@@ -0,0 +1,15 @@
+def file = new File('esxcli_hardware.xml')
+
+def results = new XmlSlurper().parse(file).declareNamespace(['esxcli': 'http://www.vmware.com/Products/ESX/5.0/esxcli/'])
+
+results = results.'root'.'list'.'structure'
+
+def result = results.find { it.'field'.'string' =~ /OpenManage/ }
+
+def result_fields = result.field
+Map res = [:]
+result_fields.each {
+    res << ["${it.'@name'}": it]
+}
+
+println res
